ecec3f616f5e6d401fa1279da0962ac3568b049a
Администратор <@PC2>
2021-01-06 22:29:39 +0200

    Start up setting add

--- tutor_schedule/Program.cs
+++ tutor_schedule/Program.cs
@@ -8,8 +8,20 @@ namespace tutor_schedule
 {
     class Program
     {
+        static Telegram.Bot.TelegramBotClient botClient = null;
         static void Main(string[] args)
         {
+            botClient = new Telegram.Bot.TelegramBotClient("1540919518:AAGdQsfV0cugPqldiKTCooHs5aqBURKNQv0");
+
+            botClient.OnMessage += BotClient_OnMessageAsync;
+
+            botClient.StartReceiving();
+            Console.Read();
+        }
+
+        private static async void BotClient_OnMessageAsync(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            await botClient.SendTextMessageAsync(e.Message.Chat.Id, "hello!");
         }
     }
 }
